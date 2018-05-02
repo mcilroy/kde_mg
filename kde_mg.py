e9589f83df4a6949872199f7a60c057ea4e8151d
mcilroy <mcilroy23@gmail.com>
2018-05-02 17:10:03 -0300

    experimented with further optimization

--- kde_mg.py
+++ kde_mg.py
@@ -9,14 +9,46 @@ def mean_log_prob(data_a, data_b, std_dev):
     std_2 = -2 * np.power(std_dev, 2)
     pi_stddev_2 = np.log(2 * np.pi * np.power(std_dev, 2)) / 2
     summ_m = 0.0
+    len_data_a = data_a.shape[0]
     for i in range(data_b.shape[0]):
-        summ_k = np.empty((data_a.shape[0]))
-        for j in range(data_a.shape[0]):
+        summ_k = np.empty(len_data_a)
+        for j in range(len_data_a):
             p_x_z = (np.power(data_b[i] - data_a[j], 2) / std_2) - pi_stddev_2
             summ_k[j] = log_k + p_x_z.sum()
+        m = np.max(summ_k)
+        summ_k = np.exp(summ_k - m)
+        lg_prob = np.log(summ_k.sum()) + m
+        summ_m += lg_prob
+
+        if i % 1000 == 0:
+            print(i)
+    return summ_m/data_b.shape[0]
+
+
+def mean_log_prob3(data_a, data_b, std_dev):
+    """ calculates mean log probability of data_b on data_a
+    use numerical stability trick for log(exp(a)+exp(b))
+    to speed up the summations data_b is duplicated and data_a is subtracted from it.
+    """
+    log_k = np.log(1 / data_a.shape[0])
+    std_2 = -2 * np.power(std_dev, 2)
+    pi_stddev_2 = np.log(2 * np.pi * np.power(std_dev, 2)) / 2
+    summ_m = 0.0
+    for i in range(data_b.shape[0]):
+        #duplicated_data_b = np.repeat(data_b[i], data_a.shape[0])
+        #duplicated_data_b = np.reshape(duplicated_data_b, data_a.shape)
+        duplicated_data_b = np.tile(data_b[i], (data_a.shape[0], 1))
+        p_x_z = (np.power(duplicated_data_b - data_a, 2) / std_2) - pi_stddev_2
+        summ_k = log_k + p_x_z.sum(axis=1)
         m = np.max(summ_k)
         summ_k = summ_k - m
         summ_k = np.exp(summ_k)
         lg_prob = np.log(summ_k.sum()) + m
         summ_m += lg_prob
+
+        #summ_m += np.log(np.exp(summ_k).sum())
+
+        if i % 1000 == 0:
+            print("i: " + str(i))
     return summ_m/data_b.shape[0]
+
--- search.py
+++ search.py
@@ -13,10 +13,10 @@ def main():
 def mnist():
     mnist_train, mnist_val, mnist_test = dataset.read_mnist_data('.')
     dataset.display_mnist(mnist_train[:200])
-    best_std_dev, best_log_probs = find_optimal_value(mnist_train[0:10], mnist_val[0:10])
+    best_std_dev, best_log_probs = find_optimal_value(mnist_train[:10], mnist_val[:10], "mnist")
     print("Best std dev: " + str(best_std_dev) + " achieved with a mean log probability of: " + str(best_log_probs))
     t0 = time.time()
-    mean_log_prob = kde_mg.mean_log_prob(mnist_train[0:100], mnist_test[0:100], best_std_dev)
+    mean_log_prob = kde_mg.mean_log_prob(mnist_train[:200], mnist_test, best_std_dev)
     total_time = time.time() - t0
     print("Calculating mean log probability of test mnist dataset...")
     print("Running time: " + str(total_time) + " seconds")
@@ -26,34 +26,37 @@ def mnist():
 def cifar100():
     cifar100_train, cifar100_val, cifar100_test = dataset.read_cifar100('cifar-100-python')
     dataset.display_cifar100(cifar100_train[:200])
-    best_std_dev, best_log_probs = find_optimal_value(cifar100_train[0:10], cifar100_val[0:10])
+    best_std_dev, best_log_probs = find_optimal_value(cifar100_train, cifar100_val, "cifar100")
     print("Best std dev: " + str(best_std_dev) + " achieved with a mean log probability of: " + str(best_log_probs))
     t0 = time.time()
-    mean_log_prob = kde_mg.mean_log_prob(cifar100_train[0:100], cifar100_test[0:100], best_std_dev)
+    mean_log_prob = kde_mg.mean_log_prob(cifar100_train, cifar100_test, best_std_dev)
     total_time = time.time() - t0
     print("Calculating mean log probability of test CIFAR100 dataset...")
     print("Running time: " + str(total_time) + " seconds")
     print("Mean log probability on CIFAR100 test data: " + str(mean_log_prob))
 
 
-def find_optimal_value(train, val):
+def find_optimal_value(train, val, dataset_name):
     std_devs = [0.05, 0.08, 0.1, 0.2, 0.5, 1.0, 1.5, 2.0]
     mean_log_probs = []
     for i, std_dev in enumerate(std_devs):
         mean_log_prob = kde_mg.mean_log_prob(train, val, std_dev)
         mean_log_probs.append(mean_log_prob)
         print(i)
-    visualization_of_optimal_value(std_devs, mean_log_probs)
+    visualization_of_optimal_value(std_devs, mean_log_probs, dataset_name)
     best_log_probs = np.amax(mean_log_probs)
     best_std_dev = std_devs[np.argmax(mean_log_probs)]
     return best_std_dev, best_log_probs
 
 
-def visualization_of_optimal_value(std_devs, mean_log_probs):
+def visualization_of_optimal_value(std_devs, mean_log_probs, dataset_name):
     plt.plot(std_devs, mean_log_probs)
     plt.xlabel("standard deviation")
     plt.ylabel("mean log probability")
-    plt.show()
+    #plt.show()
+    plt.savefig(dataset_name+'.png')
+
 
 if __name__ == "__main__":
+    np.random.seed(1)
     main()
