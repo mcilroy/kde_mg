e39df9f147a2bac12dc8c32e8fc3d1a161a97305
mcilroy <mcilroy23@gmail.com>
2018-05-03 22:01:01 -0300

    added multi-proc optimization

--- kde_mg.py
+++ kde_mg.py
@@ -1,62 +1,13 @@
 import numpy as np
 from multiprocessing import cpu_count, Pool
 
-""" different methods to calculate mean log probability. Each is successfully more optimized at the expense of 
-readability"""
-
-
-def mean_log_prob(data_a, data_b, std_dev):
-    """ calculates mean log probability of data_b on data_a
-    use numerical stability trick for log(exp(a)+exp(b))
-    """
-    log_k = np.log(1 / data_a.shape[0])
-    std_2 = -2 * np.power(std_dev, 2)
-    pi_stddev_2 = np.log(2 * np.pi * np.power(std_dev, 2)) / 2
-    summ_m = 0.0
-    len_data_a = data_a.shape[0]
-    for i in range(data_b.shape[0]):
-        summ_k = np.empty(len_data_a)
-        for j in range(len_data_a):
-            p_x_z = (np.power(data_b[i] - data_a[j], 2) / std_2) - pi_stddev_2
-            summ_k[j] = log_k + p_x_z.sum()
-        m = np.max(summ_k)
-        summ_k = np.exp(summ_k - m)
-        lg_prob = np.log(summ_k.sum()) + m
-        summ_m += lg_prob
-
-        if i % 1000 == 0:
-            print("i: " + str(i))
-    return summ_m/data_b.shape[0]
-
-
-def mean_log_prob_tiling(data_a, data_b, std_dev):
-    """ calculates mean log probability of data_b on data_a
-    use numerical stability trick for log(exp(a)+exp(b))
-    to speed up the summations data_b is duplicated and data_a is subtracted from it.
-    """
-    log_k = np.log(1 / data_a.shape[0])
-    std_2 = -2 * np.power(std_dev, 2)
-    pi_stddev_2 = np.log(2 * np.pi * np.power(std_dev, 2)) / 2
-    summ_m = 0.0
-    for i in range(data_b.shape[0]):
-        duplicated_data_b = np.tile(data_b[i], (data_a.shape[0], 1))
-        p_x_z = (np.power(duplicated_data_b - data_a, 2) / std_2) - pi_stddev_2
-        summ_k = log_k + p_x_z.sum(axis=1)
-        m = np.max(summ_k)
-        summ_k = summ_k - m
-        summ_k = np.exp(summ_k)
-        lg_prob = np.log(summ_k.sum()) + m
-        summ_m += lg_prob
-
-        if i % 1000 == 0:
-            print("i: " + str(i))
-    return summ_m/data_b.shape[0]
+""" method to calculate mean log likelihood."""
 
 
 def mean_log_prob_tiling_multi_proc(data_a, data_b, std_dev):
-    """ calculates mean log probability of data_b on data_a
+    """ calculates mean log likelihood of data_b on data_a
     use numerical stability trick for log(exp(a)+exp(b))
-    to speed up the summations data_b is duplicated and data_a is subtracted from it.
+    to speed up the summations data_b[i] is duplicated into a matrix and data_a is subtracted from it.
     Use multi-processing to assign a core to each inner summation. Noticeable speed up at large problem size.
     """
     pool = Pool(cpu_count())
--- search.py
+++ search.py
@@ -5,11 +5,11 @@ import time
 import matplotlib.pyplot as plt
 
 """
-The goal of the program is to calculate the mean log probability of a dataset B on dataset A.
-The model is a mixture of gaussians where each gaussian componenet is centered around a data point in A.
+The goal of the program is to calculate the mean log likelihood of a dataset B on dataset A.
+The model is a mixture of gaussians where each gaussian component is centered around a data point in A.
 The program loads MNIST and CIFAR100, splits them into train, validation and testing then optimizes the choice of 
 standard deviation by running the validation on the training. 
-Finally the testing dataset's mean log probability is calculated on the training. 
+Finally the testing dataset's mean log likelihood is calculated on the training. 
 """
 
 
@@ -19,44 +19,44 @@ def main():
 
 
 def mnist():
-    """ Load MNIST data. Optimize the standard deviation. Calculate mean log probability of the mnist testing dataset. 
+    """ Load MNIST data. Optimize the standard deviation. Calculate mean log likelihood of the mnist testing dataset. 
     Benchmarked running time."""
     mnist_train, mnist_val, mnist_test = dataset.read_mnist_data('.')
     dataset.display_mnist(mnist_train[:200])
     best_std_dev, best_log_probs = find_optimal_value(mnist_train, mnist_val, "mnist")
-    print("Best std dev: " + str(best_std_dev) + " achieved with a mean log probability of: " + str(best_log_probs))
+    print("Best std dev: " + str(best_std_dev) + " achieved with a mean log likelihood of: " + str(best_log_probs))
     t0 = time.time()
     mean_log_prob = kde_mg.mean_log_prob_tiling_multi_proc(mnist_train, mnist_test, best_std_dev)
     total_time = time.time() - t0
-    print("Calculating mean log probability of test mnist dataset...")
+    print("Calculating mean log likelihood of test mnist dataset...")
     print("Running time: " + str(total_time) + " seconds")
-    print("Mean log probability on mnist test data: " + str(mean_log_prob))
+    print("Mean log likelihood on mnist test data: " + str(mean_log_prob))
 
 
 def cifar100():
-    """ Load CIFAR100 data. Optimize the standard deviation. Calculate mean log probability of the CIFAR100 testing 
+    """ Load CIFAR100 data. Optimize the standard deviation. Calculate mean log likelihood of the CIFAR100 testing 
     dataset. Benchmarked running time."""
     cifar100_train, cifar100_val, cifar100_test = dataset.read_cifar100('cifar-100-python')
     dataset.display_cifar100(cifar100_train[:200])
     best_std_dev, best_log_probs = find_optimal_value(cifar100_train, cifar100_val, "cifar100")
-    print("Best std dev: " + str(best_std_dev) + " achieved with a mean log probability of: " + str(best_log_probs))
+    print("Best std dev: " + str(best_std_dev) + " achieved with a mean log likelihood of: " + str(best_log_probs))
     t0 = time.time()
     mean_log_prob = kde_mg.mean_log_prob_tiling_multi_proc(cifar100_train, cifar100_test, best_std_dev)
     total_time = time.time() - t0
-    print("Calculating mean log probability of test CIFAR100 dataset...")
+    print("Calculating mean log likelihood of test CIFAR100 dataset...")
     print("Running time: " + str(total_time) + " seconds")
-    print("Mean log probability on CIFAR100 test data: " + str(mean_log_prob))
+    print("Mean log likelihood on CIFAR100 test data: " + str(mean_log_prob))
 
 
 def find_optimal_value(train, val, dataset_name):
-    """ For each std dev calculate mean log probability for validation on training. Select std dev with highest 
-    log probability."""
+    """ For each std dev calculate mean log likelihood for validation on training. Select std dev with highest 
+    log likelihood."""
     std_devs = [0.05, 0.08, 0.1, 0.2, 0.5, 1.0, 1.5, 2.0]
     mean_log_probs = []
     for i, std_dev in enumerate(std_devs):
+        print(dataset_name + " - finding optimal value % complete: " + str((float(i) / len(std_devs)) * 100))
         mean_log_prob = kde_mg.mean_log_prob_tiling_multi_proc(train, val, std_dev)
         mean_log_probs.append(mean_log_prob)
-        print(dataset_name + " - finding optimal value % complete: " + str((float(i)/len(std_devs))*100))
     visualization_of_optimal_value(std_devs, mean_log_probs, dataset_name)
     best_log_probs = np.amax(mean_log_probs)
     best_std_dev = std_devs[np.argmax(mean_log_probs)]
@@ -64,11 +64,11 @@ def find_optimal_value(train, val, dataset_name):
 
 
 def visualization_of_optimal_value(std_devs, mean_log_probs, dataset_name):
-    """ Visualize optimal value with a plot of the different log probabilities for each std dev."""
+    """ Visualize optimal value with a plot of the different log likelihoods for each std dev."""
     plt.figure()
     plt.plot(std_devs, mean_log_probs)
     plt.xlabel("standard deviation")
-    plt.ylabel("mean log probability")
+    plt.ylabel("mean log likelihood")
     plt.savefig(dataset_name+'_optimal_value_curve.png')
 
 
